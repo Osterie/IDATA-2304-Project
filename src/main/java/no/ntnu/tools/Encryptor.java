945f8dcec7323b4853b851e889c6b9cced291962
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2024-10-01 17:37:02 +0200

    Made base classes for encryption

---
+++ src/main/java/no/ntnu/tools/Encryptor.java
@@ -0,0 +1,27 @@
+package no.ntnu.tools;
+
+/**
+ * This class is used to encrypt and decrypt string messages
+ */
+public class Encryptor {
+
+    /**
+     * This method takes in a message as a string
+     * and gives back an encrypted string.
+     */
+    public static String encryptString(String message) {
+        String encryptedMessage = "";
+
+        return encryptedMessage;
+    }
+
+    /**
+     * This method takes in an encrypted string
+     * and gives back a decrypted string.
+     */
+    public static String dencryptString(String encryptedMessage) {
+        String decryptedMessage = "";
+
+        return decryptedMessage;
+    }
+}
