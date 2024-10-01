8e18550e23e2e1967050621e13ba76d3c1a7162b
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2024-10-01 17:19:00 +0200

    Added base classes for commands and messages

---
+++ src/main/java/no/ntnu/commands/Filler.java
@@ -0,0 +1,7 @@
+package no.ntnu.commands;
+
+/**
+ * This class will be replaced by logic class later
+ */
+public class Filler {
+}
---
+++ src/main/java/no/ntnu/commands/Message.java
@@ -0,0 +1,8 @@
+package no.ntnu.commands;
+
+/**
+ * An abstract message sent or received by the server.
+ */
+public interface Message {
+
+}
