f32c89ca5269bd1f4c48021e61a990c08538e111
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2024-10-14 10:03:55 +0200

    Added directory for test classes

---
+++ src/test/java/no/ntnu/tools/Encryption/CeasarCipherEncryptor.java
@@ -0,0 +1,5 @@
+package no.ntnu.tools.Encryption;
+
+public class CeasarCipherEncryptor {
+
+}
---
+++ src/test/java/no/ntnu/tools/Encryption/HashEncryptor.java
@@ -0,0 +1,8 @@
+package no.ntnu.tools.Encryption;
+
+/**
+ * This class is used to hash strings.
+ */
+public class HashEncryptor {
+
+}
