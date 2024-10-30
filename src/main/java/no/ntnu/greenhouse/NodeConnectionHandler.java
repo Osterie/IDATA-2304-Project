fcb4441ed880fc24ca72504082f0b64d0bd490f2
OSTERITE <adrian@bandidos.org>
2024-10-30 12:35:16 +0100

    Fixed merge conflict...

--- src/main/java/no/ntnu/greenhouse/NodeConnectionHandler.java
+++ src/main/java/no/ntnu/greenhouse/NodeConnectionHandler.java
@@ -10,24 +10,12 @@ import java.util.HashMap;
 import no.ntnu.Clients;
 import no.ntnu.tools.Logger;
 
-/**
- * Handles the connection between a SensorActuatorNode and the server.
- * This class manages the communication, sending sensor data, and handling server commands.
- */
 public class NodeConnectionHandler implements Runnable {
     private final SensorActuatorNode node;
     private final Socket socket;
     private final PrintWriter socketWriter;
     private final BufferedReader socketReader;
 
-    /**
-     * Constructs a NodeConnectionHandler for a given node, host, and port.
-     *
-     * @param node The SensorActuatorNode to be managed
-     * @param host The server host
-     * @param port The server port
-     * @throws IOException If an I/O error occurs when creating the socket or streams
-     */
     public NodeConnectionHandler(SensorActuatorNode node, String host, int port) throws IOException {
         this.node = node;
         this.socket = new Socket(host, port);
@@ -41,9 +29,6 @@ public class NodeConnectionHandler implements Runnable {
         Logger.info("connecting node " + node.getId() + " with identifier: " + identifierMessage);
     }
 
-    /**
-     * Runs the NodeConnectionHandler thread, listening for messages from the server and handling them.
-     */
     @Override
     public void run() {
         try {
@@ -52,7 +37,8 @@ public class NodeConnectionHandler implements Runnable {
                 if (serverMessage != null) {
                     Logger.info("Received for node " + node.getId() + ": " + serverMessage);
                     handleServerCommand(serverMessage);
-                } else {
+                }
+                else{
                     Logger.info("Invalid request from server: " + serverMessage);
                 }
             }
@@ -63,20 +49,10 @@ public class NodeConnectionHandler implements Runnable {
         }
     }
 
-    /**
-     * Sends sensor data to the server.
-     *
-     * @param data The sensor data to send
-     */
     public void sendSensorData(String data) {
         socketWriter.println(data);
     }
 
-    /**
-     * Handles commands received from the server.
-     *
-     * @param command The command received from the server
-     */
     private void handleServerCommand(String command) {
         Logger.info("Received command for node! " + node.getId() + ": " + command);
         String[] commandParts = command.split("-");
@@ -93,32 +69,58 @@ public class NodeConnectionHandler implements Runnable {
 
         if (commandType.equalsIgnoreCase("GET_NODE_ID")) {
             Logger.info("Received request for node ID from server, sending response " + node.getId());
+            // socketWriter.println(sender + ";" + senderID + ";" + node.getId());
             socketWriter.println(Clients.CONTROL_PANEL + ";0-GET_NODE_ID;" + node.getId()); // TODO change the id. (from 0)
-        } else if (commandType.equalsIgnoreCase("GET_NODE")) {
+        }
+        else if (commandType.equalsIgnoreCase("GET_NODE")){
             Logger.info("Received request for node from server, sending response " + sender + ";" + senderID + ";" + node.getId());
+            // spawner.spawnNode("4;2_heater;3_window", START_DELAY + 3);
 
+            // List<Sensor> sensors = node.getSensors();
             ActuatorCollection actuators = node.getActuators();
 
+            // TODO send state of actuator, on/off?
+
             StringBuilder actuatorString = new StringBuilder();
+            HashMap<String, Integer> actuatorCount = new HashMap<String, Integer>();
             for (Actuator actuator : actuators) {
                 actuatorString.append(";" + actuator.getType() + "_" + actuator.getId());
+                // if (actuatorCount.containsKey(actuator.getType())) {
+                //     actuatorCount.put(actuator.getType(), actuatorCount.get(actuator.getType()) + 1);
+                // } else {
+                //     actuatorCount.put(actuator.getType(), 1);
+                // }
+                // actuatorString.append(actuator "_" + actuator.getType());
             }
 
+            // for (String key : actuatorCount.keySet()) {
+            //     actuatorString.append(";" + actuatorCount.get(key) + "_" + key);
+            // }
+
             String resultString = actuatorString.toString();
+
+
+            Logger.info("Received request for node from server, sending response " + sender + ";" + senderID + ";" + node.getId());
+            // socketWriter.println(sender + ";" + senderID + ";" + node.getId());
             Logger.info(resultString);
             socketWriter.println(Clients.CONTROL_PANEL + ";0-GET_NODE;" + node.getId() + resultString); // TODO add sensor data and actuator data.
-        } else if (commandType.equalsIgnoreCase("ACTUATOR_CHANGE")) {
+        }
+        else if (commandType.equalsIgnoreCase("ACTUATOR_CHANGE")){
             int actuatorId = Integer.parseInt(bodyParts[1]);
             boolean isOn = bodyParts[2].equalsIgnoreCase("ON");
             node.setActuator(actuatorId, isOn);
-        } else {
+        }
+        else{
             Logger.info("Received unknown command from server: " + command);
         }
+
+        // spawner.advertiseSensorData("4;temperature=27.4 °C,temperature=26.8 °C,humidity=80 %", START_DELAY + 2);
+
+
+        // Parse and execute commands received from the server for this node
+        // Example: Control actuators based on command type
     }
 
-    /**
-     * Closes the connection to the server, including the socket and streams.
-     */
     public void closeConnection() {
         try {
             socket.close();
