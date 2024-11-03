a4a3ecdb41b816d57df8bf32c206617de20e2e6f
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2024-11-03 13:57:42 +0100

    Reverted NodeConnectionHandler class due to performance issues

--- src/main/java/no/ntnu/greenhouse/NodeConnectionHandler.java
+++ src/main/java/no/ntnu/greenhouse/NodeConnectionHandler.java
@@ -1,21 +1,27 @@
 package no.ntnu.greenhouse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.HashMap;
 
 import no.ntnu.Clients;
-import no.ntnu.SocketCommunicationChannel;
-import no.ntnu.messages.MessageBody;
-import no.ntnu.messages.MessageHeader;
-import no.ntnu.messages.MessageTest;
 import no.ntnu.tools.Logger;
 
-public class NodeConnectionHandler extends SocketCommunicationChannel implements Runnable {
+public class NodeConnectionHandler implements Runnable {
     private final SensorActuatorNode node;
+    private final Socket socket;
+    private final PrintWriter socketWriter;
+    private final BufferedReader socketReader;
 
     public NodeConnectionHandler(SensorActuatorNode node, String host, int port) throws IOException {
-        super(host, port);
         this.node = node;
+        this.socket = new Socket(host, port);
+        this.socket.setKeepAlive(true);
+        this.socketWriter = new PrintWriter(socket.getOutputStream(), true);
+        this.socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         // Send initial identifier to server
         String identifierMessage = "GREENHOUSE;" + node.getId();
@@ -25,38 +31,46 @@ public class NodeConnectionHandler extends SocketCommunicationChannel implements
 
     @Override
     public void run() {
-        this.listenForMessages();
+        try {
+            while (!socket.isClosed()) {
+                String serverMessage = socketReader.readLine();
+                if (serverMessage != null) {
+                    Logger.info("Received for node " + node.getId() + ": " + serverMessage);
+                    handleServerCommand(serverMessage);
+                }
+                else{
+                    Logger.info("Invalid request from server: " + serverMessage);
+                }
+            }
+        } catch (IOException e) {
+            Logger.error("Connection lost for node " + node.getId() + ": " + e.getMessage());
+        } finally {
+            this.close();
+        }
     }
 
     public void sendSensorData(String data) {
         socketWriter.println(data);
     }
 
-    @Override
-    protected void handleMessage(String serverMessage) {
-        
-        Logger.info("Received message for node! " + node.getId() + ": " + serverMessage);
+    private void handleServerCommand(String command) {
+        Logger.info("Received command for node! " + node.getId() + ": " + command);
+        String[] commandParts = command.split("-");
 
-        MessageTest message = MessageTest.fromProtocolString(serverMessage);
+        String header = commandParts[0];
+        String body = commandParts[1];
 
-        MessageHeader header = message.getHeader();
-        MessageBody body = message.getBody();
+        String[] headerParts = header.split(";");
+        String[] bodyParts = body.split(";");
 
-        Clients sender = header.getReceiver();
-        String senderID = header.getId();
-        String commandType = body.getCommand();
-
-        Logger.info("Received message from " + sender + " with ID " + senderID + " and command " + commandType);
+        String sender = headerParts[0];
+        String senderID = headerParts[1];
+        String commandType = bodyParts[0];
 
         if (commandType.equalsIgnoreCase("GET_NODE_ID")) {
             Logger.info("Received request for node ID from server, sending response " + node.getId());
-
-            // TODO change the id. (from 0)
-            MessageHeader responseHeader = new MessageHeader(Clients.CONTROL_PANEL, "0", "STRING");
-            MessageBody responseBody = new MessageBody("GET_NODE_ID;" + node.getId());
-            MessageTest responseMessage = new MessageTest(responseHeader, responseBody);
-
-            socketWriter.println(responseMessage.toProtocolString());
+            // socketWriter.println(sender + ";" + senderID + ";" + node.getId());
+            socketWriter.println(Clients.CONTROL_PANEL + ";0-GET_NODE_ID;" + node.getId()); // TODO change the id. (from 0)
         }
         else if (commandType.equalsIgnoreCase("GET_NODE")){
             Logger.info("Received request for node from server, sending response " + sender + ";" + senderID + ";" + node.getId());
@@ -91,16 +105,13 @@ public class NodeConnectionHandler extends SocketCommunicationChannel implements
             Logger.info(resultString);
             socketWriter.println(Clients.CONTROL_PANEL + ";0-GET_NODE;" + node.getId() + resultString); // TODO add sensor data and actuator data.
         }
-        else if (commandType.contains("ACTUATOR_CHANGE")){
-            Logger.info("Received actuator change command from server: " + serverMessage + " " + body.getCommand());
-
-            String[] bodyParts = body.toProtocolString().split(";");
+        else if (commandType.equalsIgnoreCase("ACTUATOR_CHANGE")){
             int actuatorId = Integer.parseInt(bodyParts[1]);
             boolean isOn = bodyParts[2].equalsIgnoreCase("ON");
             node.setActuator(actuatorId, isOn);
         }
         else{
-            Logger.info("Received unknown command from server: " + serverMessage);
+            Logger.info("Received unknown command from server: " + command);
         }
 
         // spawner.advertiseSensorData("4;temperature=27.4 °C,temperature=26.8 °C,humidity=80 %", START_DELAY + 2);
@@ -108,5 +119,16 @@ public class NodeConnectionHandler extends SocketCommunicationChannel implements
 
         // Parse and execute commands received from the server for this node
         // Example: Control actuators based on command type
+    }
+
+    public void close() {
+        try {
+            socket.close();
+            socketWriter.close();
+            socketReader.close();
+            Logger.info("Connection closed for node " + node.getId());
+        } catch (IOException e) {
+            Logger.error("Error closing connection for node " + node.getId() + ": " + e.getMessage());
+        }
     }
 }
