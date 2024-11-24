3a82cd7640a9219b0a4daaffbe0f294b4543a297
knut <knutolav01@gmail.com>
2024-11-24 15:02:35 +0100

    add ActuatorChangeCommandTest

---
+++ src/test/java/no/ntnu/messages/ActuatorChangeCommandTest.java
@@ -0,0 +1,43 @@
+package no.ntnu.messages;
+
+import no.ntnu.greenhouse.NodeLogic;
+import no.ntnu.greenhouse.SensorActuatorNode;
+import no.ntnu.messages.commands.greenhouse.ActuatorChangeCommand;
+import no.ntnu.messages.MessageHeader;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ActuatorChangeCommandTest {
+
+    private NodeLogic nodeLogic;
+    private SensorActuatorNode node;
+    private ActuatorChangeCommand command;
+
+    @BeforeEach
+    public void setUp() {
+        nodeLogic = mock(NodeLogic.class);
+        node = mock(SensorActuatorNode.class);
+        when(nodeLogic.getNode()).thenReturn(node);
+        command = new ActuatorChangeCommand();
+    }
+
+    @Test
+    public void testExecute() {
+        MessageHeader header = mock(MessageHeader.class);
+        command.setParameters(new String[]{"1", "ON"});
+
+        Message result = command.execute(nodeLogic, header);
+
+        verify(node, times(1)).setActuator(1, true);
+        assertEquals("1;ON", result.getBody().getTransmission().getData());
+    }
+
+    @Test
+    public void testToProtocolString() {
+        command.setParameters(new String[]{"1", "ON"});
+        assertEquals("ACTUATOR_CHANGE;1;ON", command.toProtocolString());
+    }
+}
