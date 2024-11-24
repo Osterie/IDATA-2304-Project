e3ec9d82636e8738f007f20d969a4e963a846fe8
knut <knutolav01@gmail.com>
2024-11-24 15:03:52 +0100

    add GetSensorDataCommandTest

---
+++ src/test/java/no/ntnu/messages/GetSensorDataCommandTest.java
@@ -0,0 +1,37 @@
+package no.ntnu.messages;
+
+import no.ntnu.greenhouse.NodeLogic;
+import no.ntnu.messages.commands.greenhouse.GetSensorDataCommand;
+import no.ntnu.messages.MessageHeader;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class GetSensorDataCommandTest {
+
+    private NodeLogic nodeLogic;
+    private GetSensorDataCommand command;
+
+    @BeforeEach
+    public void setUp() {
+        nodeLogic = mock(NodeLogic.class);
+        command = new GetSensorDataCommand();
+    }
+
+    @Test
+    public void testExecute() {
+        MessageHeader header = mock(MessageHeader.class);
+        when(nodeLogic.getSensorData()).thenReturn("Temperature=25;Humidity=60");
+
+        Message result = command.execute(nodeLogic, header);
+
+        assertEquals("Temperature=25;Humidity=60", result.getBody().getTransmission().getData());
+    }
+
+    @Test
+    public void testToProtocolString() {
+        assertEquals("GET_SENSOR_DATA", command.toProtocolString());
+    }
+}
