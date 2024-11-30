afffd09a28cd2fe3e74781262d49599e5231508a
Daniel <danielsel@live.no>
2024-11-30 23:39:42 +0100

    gui.controlpanel now follows better google checkstyle

--- src/main/java/no/ntnu/gui/controlpanel/Ribbon.java
+++ src/main/java/no/ntnu/gui/controlpanel/Ribbon.java
@@ -5,36 +5,37 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
+
 /**
  * Factory class for creating the ribbon in the control panel.
  */
 public class Ribbon {
 
-    /**
-     * Creates the ribbon containing the menu bar with application options.
-     *
-     * @param refreshAction  The action to perform when the refresh option is clicked.
-     * @return A VBox containing the ribbon components.
-     */
-    public static VBox createRibbon(Runnable refreshAction, Runnable settingsAction) {
-        MenuBar ribbonMenuBar = new MenuBar();
+  /**
+   * Creates the ribbon containing the menu bar with application options.
+   *
+   * @param refreshAction  The action to perform when the refresh option is clicked.
+   * @return A VBox containing the ribbon components.
+   */
+  public static VBox createRibbon(Runnable refreshAction, Runnable settingsAction) {
+    MenuBar ribbonMenuBar = new MenuBar();
 
-        Menu fileMenu = new Menu("Options");
+    Menu fileMenu = new Menu("Options");
 
-        MenuItem refreshItem = new MenuItem("Refresh");
-        MenuItem exitItem = new MenuItem("Exit");
+    MenuItem refreshItem = new MenuItem("Refresh");
+    MenuItem exitItem = new MenuItem("Exit");
 
-        refreshItem.setOnAction(event -> {
-            if (refreshAction != null) {
-                refreshAction.run();
-            }
-        });
+    refreshItem.setOnAction(event -> {
+      if (refreshAction != null) {
+        refreshAction.run();
+      }
+    });
 
-        exitItem.setOnAction(event -> Platform.exit());
+    exitItem.setOnAction(event -> Platform.exit());
 
-        fileMenu.getItems().addAll(refreshItem, exitItem);
-        ribbonMenuBar.getMenus().add(fileMenu);
+    fileMenu.getItems().addAll(refreshItem, exitItem);
+    ribbonMenuBar.getMenus().add(fileMenu);
 
-        return new VBox(ribbonMenuBar);
-    }
+    return new VBox(ribbonMenuBar);
+  }
 }
