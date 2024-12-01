a2508b73e64430518547dd479fa10f7eb308a06f
Daniel <danielsel@live.no>
2024-12-01 21:49:07 +0100

    Added better checkstyle to ControlPanelStarter

--- src/main/java/no/ntnu/run/IntermediaryServerStarter.java
+++ src/main/java/no/ntnu/run/IntermediaryServerStarter.java
@@ -2,7 +2,13 @@ package no.ntnu.run;
 
 import no.ntnu.intermediaryserver.server.IntermediaryServer;
 
+/**
+ * Starter class for the intermediary server.
+ */
 public class IntermediaryServerStarter {
+    /**
+     * Entrypoint for the application.
+     */
     public static void main(String[] args) {
         // Create an instance of the IntermediaryServer.
         IntermediaryServer intermediaryServer = new IntermediaryServer();
